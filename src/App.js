f2a2fa9eafc4c24bdbc422d5e37ecab80886a51d
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2022-02-08 17:07:42 -0300

    Empezando de cero para la preentrega

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-import NavBar from './components/NavBar';
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import React, {useState, useEffect} from 'react'
 
 function App() {
-  const [cargando, setCargando] = useState(true)
-
-  useEffect(() => {
-    setTimeout(() => {
-      setCargando(false);
-    }, 2000)
-  })
-  
   return (
     <>
-      <NavBar />
-      <div className='items'>
-        {cargando ? <p>Cargando..</p> : <ItemDetailContainer />}
-      </div>
+      <h1>Hola</h1>
     </>
   );
 }
