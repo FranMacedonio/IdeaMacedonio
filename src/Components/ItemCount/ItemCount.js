673d6c101bae5bae70002f467e81984fbb674248
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2022-02-21 20:16:50 -0300

    corrigiendo desafio 10

--- src/Components/ItemCount/ItemCount.js
+++ src/Components/ItemCount/ItemCount.js
@@ -1,16 +1,13 @@
 import React, { useState } from 'react';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
-import { Link } from 'react-router-dom';
 
-const Contador = (stockItem) => {
+const Contador = () => {
 
     const [counter, setCounter] = useState(0);
 
     const sumarItem = () => {
-        if(counter < parseInt(stockItem.stock)) {
-            setCounter( counter + 1 );
-        }
+        setCounter( counter + 1 );
     }
 
     const restarItem = () => {
@@ -19,19 +16,6 @@ const Contador = (stockItem) => {
         }
     }
 
-    const click = () => {
-        if (counter !== 0){
-          let contadorO = document.getElementById('contador');
-          let botonAgregar = document.getElementById('agregar');
-          contadorO.remove();
-          botonAgregar.remove();
-          let compra = document.getElementById('compra');
-          compra.innerHTML = `<p>You got ${counter} to the cart.</p>`
-        } else {
-          alert(`You must to get more than 0 products!`)
-        }
-      };
-
     return (
         <>
             <div id='contador'>
@@ -39,9 +23,6 @@ const Contador = (stockItem) => {
                 <p>{counter}</p>
                 <button onClick={sumarItem}><AddIcon /></button>
             </div>
-            <div id='compra'></div>
-            <button id='agregar' onClick={click}>Get in the cart</button>
-            <Link to={'/cart'}><button>Purchase</button></Link>
         </>
     );
 
