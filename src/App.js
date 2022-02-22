26dd4b8bf4f742e07b398ca98ccee7129aa1bc95
Francisco Macedonio <macedoniofrancisco@hotmail.com>
2022-02-22 17:29:08 -0300

    Arrancando de cero el proyecto

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './views/home/home';
-import Men from './views/men/men';
-import Women from './views/women/women';
-import Luxary from './views/luxary/luxary';
-import ItemDetail from './Components/ItemDetail/ItemDetail';
-import Cart from './views/cart/cart';
+import React from 'react';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div id='container'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/men' element={<Men />} />
-          <Route path='/women' element={<Women />} />
-          <Route path='/luxary' element={<Luxary />} />
-          <Route path='/detail/:id' element={<ItemDetail />} />
-          <Route path='/cart' element={<Cart />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <h1>Arrancando de cero</h1>
   );
 }
 
